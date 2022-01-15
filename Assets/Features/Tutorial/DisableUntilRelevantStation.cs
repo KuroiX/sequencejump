f97233f07ddfb08bbec092016ce52f2ba3748250
Kuroi <geheeb@in.tum.de>
2022-01-15 21:36:23 +0100

    [Update] Disable undo button until relevant station

---
+++ Assets/Features/Tutorial/DisableUntilRelevantStation.cs
@@ -0,0 +1,48 @@
+using System;
+using Features.StationLogic;
+using UnityEngine;
+
+namespace Features.Tutorial
+{
+    public class DisableUntilRelevantStation : MonoBehaviour
+    {
+        [SerializeField] private GameObject disableObject;
+        
+        [SerializeField] private StationBehaviour stationBehaviour;
+
+        private Station _station;
+
+        private void Awake()
+        {
+            disableObject.SetActive(false);
+        }
+
+        private void Start()
+        {
+            _station = stationBehaviour.Station;
+        }
+
+        private void OnEnable()
+        {
+            Station.StationOpened += OnRelevantStationOpened;
+        }
+
+        private void OnRelevantStationOpened(object sender, EventArgs e)
+        {
+            Debug.Log(sender);
+            Debug.Log(_station);
+            if (sender == _station)
+            {
+                disableObject.SetActive(true);
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnDisable()
+        {
+            Station.StationOpened -= OnRelevantStationOpened;
+        }
+        
+        
+    }
+}
