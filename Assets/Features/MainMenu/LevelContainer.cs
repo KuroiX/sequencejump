0a10fdaef5906c63eb386e8742bc72a7f6645144
Kuroi <geheeb@in.tum.de>
2022-01-29 22:50:28 +0100

    [Process] Created stack menu, menu name asset and level container

---
+++ Assets/Features/MainMenu/LevelContainer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Features.MainMenu
+{
+    [CreateAssetMenu(fileName = "level", menuName = "ScriptableObject/Level", order = 0)]
+    public class LevelContainer : ScriptableObject
+    {
+        public LevelContainer prevLevel;
+        public LevelContainer nextLevel;
+        
+        public bool isUnlocked;
+        public bool isCleared;
+        public string levelName;
+        public string levelSceneName;
+    }
+}
---
+++ Assets/Features/MainMenu/MenuScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Features.MainMenu
+{
+    public class MenuScreen : MonoBehaviour
+    {
+        private static Stack<MenuScreen> _screens;
+
+        private void Awake()
+        {
+            _screens ??= new Stack<MenuScreen>();
+        }
+
+        public void Load(MenuScreen screen)
+        {
+            _screens.Push(this);
+            screen.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        
+        public void Back()
+        {
+            if (_screens.Count == 0) return;
+            _screens.Pop().gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
