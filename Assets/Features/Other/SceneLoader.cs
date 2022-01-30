701296798b34ba6adb65f20f4cfb1d6b6b61e8dd
Kuroi <geheeb@in.tum.de>
2022-01-30 22:37:06 +0100

    [Process] Added some new buttons and icons to main menu

--- Assets/Features/Other/SceneLoader.cs
+++ Assets/Features/Other/SceneLoader.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelLoader : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
