ea36a1f941dbccb9b2375a29f51858c8040c3a94
Daki <sebastian.geheeb@gmx.de>
2023-03-11 18:49:44 +0100

    [GameAward] Polished level

--- Assets/JumpEffect.cs
+++ Assets/JumpEffect.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Core.Actions;
+using Core.Queue;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class JumpEffect : MonoBehaviour
 {
     public ParticleSystem ps;
+    private ResettableQueue<ICharacterAction> _queue;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _queue = GetComponentInParent<QueueHolder>().Queue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current[Key.Space].wasPressedThisFrame)
+        if (!_queue.IsEmpty() && Keyboard.current[Key.Space].wasPressedThisFrame)
         {
             ps.Play();
         }
