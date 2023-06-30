3cf702a300e3901b79c19a656d986d8e06310a69
Julian Geheeb <geheeb@in.tum.de>
2023-06-30 22:57:44 +0200

    Created StationRefreshManager + receiver

---
+++ Assets/Features/StationLogic/StationRefreshManagerBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Features.StationLogic
+{
+    public class StationRefreshManager : MonoBehaviour
+    {
+        [SerializeField] private StationEnteredReceiverBehaviour[] stationEnteredReceivers;
+        
+        private StationBehaviour _stationBehaviour;
+
+        private void Awake()
+        {
+            _stationBehaviour = GetComponent<StationBehaviour>();
+        }
+
+        private void OnEnable()
+        {
+            Station.StationEntered += OnCurrentStationEntered;
+        }
+
+        private void OnDisable()
+        {
+            Station.StationEntered -= OnCurrentStationEntered;
+        }
+
+        private void OnCurrentStationEntered(object sender, EventArgs e)
+        {
+            if (_stationBehaviour.Station != sender) return;
+
+            foreach (var receiver in stationEnteredReceivers)
+            {
+                receiver.ReceiveStationEntered();
+            }
+        }
+    }
+}
