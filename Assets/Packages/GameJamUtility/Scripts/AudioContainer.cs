bb176c1dc9737f1816b4ddb7e74d8710af378d5b
Julian Geheeb <geheeb@in.tum.de>
2023-06-30 19:58:41 +0200

    Imported gamejam utilities

---
+++ Assets/Packages/GameJamUtility/Scripts/AudioContainer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Packages.GameJamUtility.Scripts
+{
+    public class AudioContainer : MonoBehaviour
+    {
+        public AudioClip BackgroundMusicClip => backgroundMusicClip;
+    
+        [SerializeField] private AudioClip backgroundMusicClip;
+    }
+}
