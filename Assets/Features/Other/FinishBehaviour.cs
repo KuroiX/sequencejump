bdb1900bce59b92f59f7aa8f46b510a224618475
Julian Geheeb <geheeb@in.tum.de>
2023-07-01 15:12:17 +0200

    Created finish behaviour

---
+++ Assets/Features/Other/FinishBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using Features.GameJamUtility.Scripts;
+using UnityEngine;
+
+namespace Features.Other
+{
+    public class FinishBehaviour : MonoBehaviour
+    {
+        [SerializeField] private SceneLoader sceneLoader;
+
+        private void Awake()
+        {
+            sceneLoader ??= FindObjectOfType<SceneLoader>();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            sceneLoader.LoadSceneByIndex(0);
+        }
+    }
+}
