7e23b5a3e034323b82b9b45741ef0f72097a7766
Julian Geheeb <geheeb@in.tum.de>
2023-11-04 22:29:54 +0100

    Added cycle queue functionality

---
+++ Assets/Features/QueueCycle/QueueCycleBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using Core.Actions;
+using Core.Queue;
+using UnityEngine;
+
+namespace Features.QueueCycle
+{
+    public class QueueCycleBehaviour : MonoBehaviour
+    {
+        private ResettableQueue<ICharacterAction> _queue;
+
+        private void Start()
+        {
+            _queue = FindObjectOfType<QueueHolder>().Queue;
+        }
+
+        public void CycleQueue()
+        {
+            var action = _queue.Dequeue();
+            _queue.Enqueue(action);
+        }
+    }
+}
