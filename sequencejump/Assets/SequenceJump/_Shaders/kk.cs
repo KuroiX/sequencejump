5ac6a32fb30d43ad66778696549175f8a0e894a7
Julian Geheeb <geheeb@in.tum.de>
2024-02-05 16:36:24 +0100

    Added assets and shaders

---
+++ sequencejump/Assets/SequenceJump/_Shaders/kk.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kk : MonoBehaviour
+{
+    [SerializeField] private Color color1;
+    [SerializeField] private Color color2;
+}
